669774e9be64e03915c948b2ad66b423003ea4dd
BRPeekz <pedrolpiza@yahoo.com.br>
2021-09-13 22:46:18 -0300

    Update Main
    Adicionado corpo do código

--- Main.py
+++ Main.py
@@ -1,1 +1,17 @@
-print('Projeto N1')
+import random
+
+sala = 1
+interacoes = 0
+while (interacoes < 7 and sala != 9):
+    interacoes += 1
+    if sala == 6:
+        print('Você está na sala 6, aqui há somente um caminho.\n')
+        caminho = int(input('[2] - Caminho preto\n'))
+        sala += caminho
+    elif sala == 10:
+        print('Você entrou em um portal, e foi jogado em uma sala aleatória.')
+        sala = random.randint(1,5)
+    else:
+        print('Você está na sala {}.'.format(sala))
+        caminho = int(input('[1] - Caminho vermelho\n[2] - Caminho preto\n'))
+        sala += caminho
